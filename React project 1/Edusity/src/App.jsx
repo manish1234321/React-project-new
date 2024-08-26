40db9bddb0acb098099b431bc629a728ad1c9857
Manish Goyal <manishdevesh12345@gmail.com>
2024-08-26 18:29:06 +0530

    my changes

--- React project 1/Edusity/src/App.jsx
+++ React project 1/Edusity/src/App.jsx
@@ -6,6 +6,8 @@ import Title from './Components/Title/Title'
 import About from './Components/About/About'
 import Campus from './Components/Campus/Campus'
 import Testimonials from './Components/Testimonials/Testimonials'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
@@ -16,10 +18,13 @@ const App = () => {
         <Title subTitle='Our PROGRAM' title='What We Offer'/>
       <Programs/>
       <About/>
-      <Title subTitle='GALLERY' title='Campus Photos'/>
+      <Title subTitle='GALLERY'  title='Campus Photos'/>
       <Campus/>
       <Title subTitle='TESTIMONIALS' title='What Students Says'/>
       <Testimonials/>
+      <Title subTitle='Contact Us' title='Get In Touch'/>
+      <Contact/>
+      <Footer/>
 
       </div>
       
